ee9aa46eb5cb61a6bc83857ff9c0efdf65c8bb61
Svitlana Kozeletska <svitlana_kozeletska_tp@bmc.com>
2023-05-07 00:21:37 +0300

    fix http get

--- dist/index.js
+++ dist/index.js
@@ -7096,7 +7096,7 @@ function run() {
             body: JSON.stringify(createIssueRequestBody) //'fields', createIssueRequestBody
         });
     })
-        // .then(response => response.json())
+        .then(response => response.json())
         .then(response => console.log('JIRA ticket', response))
         .catch(err => console.log(err));
 }
--- src/main.ts
+++ src/main.ts
@@ -81,7 +81,7 @@ function run(): void {
           body: JSON.stringify(createIssueRequestBody) //'fields', createIssueRequestBody
         })
     })
-    // .then(response => response.json())
+    .then(response => response.json())
     .then(response => console.log('JIRA ticket', response))
     .catch(err => console.log(err));
 }
